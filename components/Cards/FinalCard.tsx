f35daf57c622828623a20ba9872cc85bd134e25b
mariofredo <54829954+mariofredo@users.noreply.github.com>
2024-02-09 15:36:35 +0700

    style: modify style selected card cars

--- components/Cards/FinalCard.tsx
+++ components/Cards/FinalCard.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+'use client';
+import {useEffect, useState} from 'react';
 import Image from 'next/image';
 import {SelectedCarItem} from '@/types/car';
 export const FinalCard = ({
@@ -8,8 +9,28 @@ export const FinalCard = ({
   data: SelectedCarItem;
   selected: boolean;
 }) => {
+  const [viewportWidth, setViewportWidth] = useState<number>(0);
+  useEffect(() => {
+    function handleResize() {
+      setViewportWidth(window.innerWidth);
+    }
+
+    // Set initial viewport dimensions
+    handleResize();
+
+    // Add event listener to update viewport dimensions on window resize
+    window.addEventListener('resize', handleResize);
+
+    // Remove event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  useEffect(() => {
+    console.log(viewportWidth, 'viewportWidth');
+  }, [viewportWidth]);
   return (
-    <div className={`lc_fc_ctr ${selected && 'selected'}`}>
+    <div className={`lc_fc_ctr ${selected ? 'selected' : ''}`}>
       <div className='w-full  flex items-center justify-center'>
         <Image
           className='relative top-[-25px]'
@@ -27,9 +48,16 @@ export const FinalCard = ({
         <div className='lc_fc_price'>{data.price}</div>
 
         <div className='lc_fc_list_ctr'>
-          {data.specs.map((item) => (
-            <div className='fc_list_item'>{item.content}</div>
-          ))}
+          {data.specs.map((item) =>
+            viewportWidth < 768 && selected ? (
+              <div className='fc_list_ctr'>
+                <div className='fc_list_spec'>{item.spec_name}</div>
+                <div className='fc_list_spec_item'>{item.content}</div>
+              </div>
+            ) : (
+              <div className='fc_list_item'>{item.content}</div>
+            )
+          )}
         </div>
       </div>
     </div>
